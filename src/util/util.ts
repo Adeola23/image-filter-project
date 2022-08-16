f802c140a115f606be1c0e69f5262def70f7c8c4
Adeola23 <amosadeola92@gmail.com>
2022-08-16 10:55:00 +0100

    handled error

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import Jimp = require('jimp');
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -13,12 +13,12 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
     try {
       const photo = await Jimp.read(inputURL);
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpeg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
+        .write(__dirname + outpath, () => {
           resolve(__dirname + outpath);
         });
     } catch (error) {
